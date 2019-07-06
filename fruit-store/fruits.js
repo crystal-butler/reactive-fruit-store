595e126e12ed2554917559639770981675a52390
crystal-butler <crystal@crystal-butler.com>
2019-07-06 15:11:49 -0400

    added images and image description file, fruits.js

---
+++ fruit-store/fruits.js
@@ -0,0 +1,34 @@
+// fruits.js
+
+module.exports = [
+    {
+        "name": "Blowing Bubbles",
+        "price": 23.54,
+        "image": "/assets/images/bubbles.jpg",
+        "artist": "Ray Ceasar"
+    },
+    {
+        "name": "House on Fire",
+        "price": 10.33,
+        "image": "assets/images/cheetah.jpg",
+        "artist": "Richard Coleman"
+    },
+    {
+        "name": "Calling Collect",
+        "price": 8.08,
+        "image": "assets/images/collect.jpg",
+        "artist": "Reminds Me of John Nespeca"
+    },
+    {
+        "name": "This Is Your God",
+        "price": 30.87,
+        "image": "assets/images/dollar.jpg",
+        "artist": "Shepard Fairey"
+    },
+    {
+        "name": "Libertalia",
+        "price": 25.98,
+        "image": "assets/images/libertalia.jpg",
+        "artist": "Camille Rose Garcia"
+    }
+]
