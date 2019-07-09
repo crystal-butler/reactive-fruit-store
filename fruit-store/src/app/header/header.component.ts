db281bd0113bf00c77cd65936f10bf77c4b462c1
crystal-butler <crystal@crystal-butler.com>
2019-07-09 18:31:06 -0400

    add count to cart in header

--- fruit-store/src/app/header/header.component.ts
+++ fruit-store/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Product } from '../product/product.component';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<{ items: []; cart: [] }>) { 
+    store.pipe(select('shop')).subscribe(data => (this.cart = data.cart));
+  }
 
   cart = [];
   ngOnInit() {
